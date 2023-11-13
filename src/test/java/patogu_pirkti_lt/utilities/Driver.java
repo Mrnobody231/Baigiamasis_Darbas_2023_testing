b2cb548a55cff3daaa6029985d9b08e0937e234e
Mrnobody231 <andruskevic.e@gmail.com>
2023-11-13 19:30:34 +0200

    Add Driver class

--- src/test/java/patogu_pirkti_lt/utilities/Driver.java
+++ src/test/java/patogu_pirkti_lt/utilities/Driver.java
@@ -1,4 +1,31 @@
 package patogu_pirkti_lt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class Driver {
+
+    private static WebDriver driver;
+
+    public static void setChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+        driver.quit();
+    }
 }

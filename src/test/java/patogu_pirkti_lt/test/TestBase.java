9ddfb976bee5b88213604f5e84cfba1a0a9a24de
Mrnobody231 <andruskevic.e@gmail.com>
2023-11-13 19:31:07 +0200

    Add TestBase class

--- src/test/java/patogu_pirkti_lt/test/TestBase.java
+++ src/test/java/patogu_pirkti_lt/test/TestBase.java
@@ -1,4 +1,18 @@
 package patogu_pirkti_lt.test;
 
-public class TestBase {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import patogu_pirkti_lt.utilities.TestListener;
+
+@Listeners(TestListener.class)
+public abstract class TestBase {
+
+    @BeforeMethod
+    public abstract void setUp();
+
+    @AfterMethod
+    public void tearDown() {
+        Common.quitDriver();
+    }
 }
